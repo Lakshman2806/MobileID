e2c4019ab65fa2ab9a856a3c465f4bd831420c89
Lakshman <sri.lakshmanarao@students.iiit.ac.in>
2023-06-20 20:14:57 +0530

    font working

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,34 @@ import 'package:flutter/material.dart';
 // }
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('hello'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text("Hi people",
-      style: TextStyle(
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
-        letterSpacing: 2.0,
-        color: Colors.grey[600],
-        fontFamily: 'IndieFlower',
-      ),),
-    ),
-    floatingActionButton: FloatingActionButton(onPressed: null,
-      child: Text("Click"),
-      backgroundColor: Colors.indigoAccent,
-    ),
-  ),
+  home: Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hello'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text("Hi people I am Lakshman",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'DancingScript',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: null,
+        child: Text("Click me"),
+        backgroundColor: Colors.indigoAccent,
+      ),
+    );
+  }
+}
