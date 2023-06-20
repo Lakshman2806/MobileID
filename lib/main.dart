395286870f58782c6709066489635b0ee773b893
Lakshman <sri.lakshmanarao@students.iiit.ac.in>
2023-06-20 19:46:35 +0530

    font not working

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,22 @@ void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
       title: Text('hello'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body: Center(
+      child: Text("Hi people",
+      style: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 2.0,
+        color: Colors.grey[600],
+        fontFamily: 'IndieFlower',
+      ),),
+    ),
+    floatingActionButton: FloatingActionButton(onPressed: null,
+      child: Text("Click"),
+      backgroundColor: Colors.indigoAccent,
     ),
   ),
 ));
