eefa4c8d6f3c0c6384443aca150d82c98f587b96
Lakshman <sri.lakshmanarao@students.iiit.ac.in>
2023-06-25 20:24:12 +0530

    did simple ID

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,104 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(const MaterialApp(
-//     home: Text('!!!!!!Yes!!!!'),
-//   ));
-// }
-
-void main() => runApp(MaterialApp(
-  home: Home(),
+void main () => runApp(MaterialApp(
+  home: IDcard(),
 ));
 
+class IDcard extends StatelessWidget {
+  const IDcard({super.key});
 
-class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('hello'),
+        title: Text("My ID"),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.end,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20,30,20,0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.cyan,
-            child: Text("Hi1"),
-          ),
-          Container(
-            padding: EdgeInsets.all(30),
-            color: Colors.amber,
-            child: Text("Hi2"),
-          ),
-          Container(
-            padding: EdgeInsets.all(40),
-            color: Colors.red,
-            child: Text("Hi3"),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: null,
-        child: Text("Click"),
-        backgroundColor: Colors.indigoAccent,
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/L.png'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[400],
+            ),
+            Text("NAME",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text("Lakshman",
+              style: TextStyle(
+                  color: Colors.amber[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30,),
+            Text("Age",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text("18",
+              style: TextStyle(
+                  color: Colors.amber[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  'sri.lakshmanarao@students.iiit.ac.in',
+                  style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18,
+                      letterSpacing: 1.0
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 30,),
+            Text("Gender",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text("male",
+              style: TextStyle(
+                  color: Colors.amber[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
